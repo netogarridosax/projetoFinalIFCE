b8853315854ebb0cfc7c839409b2638532cc7615
Garrido Neto <134981206+netogarridosax@users.noreply.github.com>
2023-12-10 20:43:52 -0300

    Add files via upload

---
+++ git/setup.sql
@@ -0,0 +1,17 @@
+CREATE TABLE papeis (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(45)
+);
+CREATE TABLE usuarios (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(45),
+    senha VARCHAR(45),
+    id_papel INT,
+    FOREIGN KEY (id_papel) REFERENCES papeis(id)
+);
+CREATE TABLE servicos (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(45),
+    descricao VARCHAR(500),
+    quantidade INT
+);
